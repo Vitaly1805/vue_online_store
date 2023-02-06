76ef1848c826d6582fc71fb1e310a4908ba24ff6
Nosov Vitaly <vitalinosov2000@gmail.com>
2023-02-06 08:06:21 +0300

    added spoilers for filter block

--- src/components/filter/v-filter.vue
+++ src/components/filter/v-filter.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="v-filter">
     <div class="v-filter__body">
-      <vSliderPrice 
-        @setRangePrice="setRangePrice"
-        @filterProducts="filterProducts"
-      />
-      <vManufacturer
-        @changeManufacturers="changeManufacturers"
-      />
+      <div class="v-filter__list">
+        <div class="v-filter__item v-filter__item_active">
+          <div 
+            class="v-filter__subtitle"
+            @click="toggleShowSpoiler">
+            Цена (руб.)
+          </div>
+          <div class="v-filter__item-body">
+            <vSliderPrice 
+              @setRangePrice="setRangePrice"
+              @filterProducts="filterProducts"
+            />
+          </div>
+        </div>
+        <div class="v-filter__item">
+          <div 
+            class="v-filter__subtitle"
+            @click="toggleShowSpoiler">
+            Производители
+          </div>
+          <div class="v-filter__item-body">
+            <vManufacturer
+              @changeManufacturers="changeManufacturers"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,7 +54,8 @@ export default {
     data() {
       return {
         filterOptions: {},
-        manufacturers: []
+        manufacturers: [],
+        isShowSpoilers: [true, false]
       }
     },
     methods: {
@@ -68,6 +89,10 @@ export default {
       changeManufacturers(manufacturers) {
         this.manufacturers = manufacturers
         this.filterProducts()
+      },
+      toggleShowSpoiler(event) {
+        const item = event.target.closest('.v-filter__item')
+        item.classList.toggle('v-filter__item_active')
       }
     },
     computed: {
@@ -78,7 +103,6 @@ export default {
 
 <style lang="scss">
 .v-filter {
-  padding: 10px 5px;
   width: 300px;
   border: 1px solid #cdcdcd;
   position: relative;
@@ -86,11 +110,66 @@ export default {
   &__body {
     display: flex;
     flex-direction: column;
-    gap: 20px 0;
   }
 
   &__button {
     margin: 20px auto 0 auto;
+  }
+
+  &__item {
+    &_active {
+      .v-filter__subtitle {
+        &::after {
+          right: 9px;
+          top: 50%;
+        }
+
+        &::before {
+          top: 50%;
+          transform: rotate(135deg);
+        }
+      }
+
+      .v-filter__item-body {
+        display: block;
+      }
+    }
+
+    &-body {
+      padding: 0 10px;
+      display: none;
+    }
+  }
+
+  &__subtitle {
+    cursor: pointer;
+    margin: 0 0 10px 0;
+    padding: 10px;
+    font-size: .9em;
+    text-align: left;
+    border-top: 1px solid #cdcdcd; 
+    border-bottom: 1px solid #cdcdcd; 
+    position: relative;
+
+    &::after,
+    &::before {
+      content: '';
+      position: absolute;
+      right: 5px;
+      width: 6px;
+      height: 2px;
+      background: #000;
+    }
+
+    &::after {
+      transform: rotate(45deg);
+      top: calc(50% - 2px);
+    }
+
+    &::before {
+      transform: rotate(-45deg);
+      top: calc(50% + 2px);
+    }
   }
 }
 
